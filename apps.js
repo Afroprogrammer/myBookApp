d99672bc789a1cd1c69aa3d43c18055b002fc3d5
afroprogrammer <forsonfranklin@gmail.com>
2020-07-26 08:08:48 +0000

    added a UI class with a static method that displays books hard coded in the array for creating purposes

--- apps.js
+++ apps.js
@@ -7,3 +7,25 @@ class Book{
         this.isbn;
   }
 }
+
+//UI Class: Handle UI Tasks
+ class UI{
+     static displayBooks(){
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'Kojo John Snow',
+                isbn: '112244'
+            },
+            {
+                title: 'American Gods',
+                author: 'Kojo John Snow',
+                isbn: '11224455'
+            },
+        ] ;
+
+        const books = StoredBooks;
+     }
+ }
+
+//Store Class: Handles Storage
